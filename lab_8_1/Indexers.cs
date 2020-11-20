650ef7a834cee66082bbd2933ffd0db78b9d214a
v-andrusenko <andrusenko.vp1@gmail.com>
2020-11-20 20:54:14 +0200

    the first task

---
+++ lab_8_1/Indexers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_8_1
+{
+    class Indexers
+    {
+        string x, y, Else = "Опа! Элемента с данным индексом не существует."; 
+
+        public Indexers(string theFirstValue, string theSecondValue)
+        {
+            this.x = theFirstValue;
+            this.y = theSecondValue;
+        }
+
+        
+        public string this[int index]
+        {
+
+            get 
+            { 
+                if (index == 0)
+                {
+                    return x;
+                }
+                if (index == -1)
+                {
+                    return y;
+                }
+                else
+                {
+                    return Else;
+                }
+            }
+            set
+            {
+                if (index ==0)
+                {
+                    x = value;
+                }
+                else if (index ==-1)
+                {
+                    y = value;
+                }
+                else
+                {
+                    Else = value;
+                }
+
+            }
+        }
+    }
+}
---
+++ lab_8_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.Write("Введите значение для первого поля: ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                string theFirst = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+
+                Console.Write("Введите значение для второго поля: ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                string theSecond = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+
+                Indexers newindexer = new Indexers(theFirst, theSecond);
+                Console.WriteLine();
+
+
+                Console.Write("Введите индекс (0 и -1 - вывод значений поля): ");
+                int index = int.Parse(Console.ReadLine());
+
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(newindexer[index]);
+                Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
