c5a7dcb33ef414053a0b0b7efc38fdcfe9868862
v-andrusenko <andrusenko.vp1@gmail.com>
2020-11-21 15:06:51 +0200

    update

--- lab_8_2/DateIndexer.cs
+++ lab_8_2/DateIndexer.cs
@@ -6,18 +6,13 @@ namespace lab_8_2
 {
     class DateIndexer
     {
-        int day;
-        int month;
-        int year;
+
         public DateTime newDate;
 
         public DateIndexer() { }
 
         public DateIndexer(int day, int month, int year)
         {
-            this.day = day;
-            this.month = month;
-            this.year = year;
             newDate = new DateTime(year, month, day);
         }
 
---
+++ lab_8_3/BaseClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_8_3
+{
+    class BaseClass
+    {
+        protected int[] array;
+    }
+}
